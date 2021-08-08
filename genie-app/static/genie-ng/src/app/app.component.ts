dcd87fde520d58d0108ca68628f77b6f6c3b5ec7
Ali Akhtar <ali@ali.actor>
2021-08-09 01:21:05 +0500

    Calling invoke from angular working

--- genie-app/static/genie-ng/src/app/app.component.ts
+++ genie-app/static/genie-ng/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { invoke } from '@forge/bridge';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'genie-ng';
+
+  ngOnInit() {
+    console.log("In init, invoking...");
+    invoke('getText', { example: 'my-invoke-variable' }).then(result => {
+      console.log("Received invoke result", result);
+    });
+  }
 }
