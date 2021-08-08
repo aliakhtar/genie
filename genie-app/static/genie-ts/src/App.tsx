a78ecef68034cdf692d1e41d940e3082636fb98f
Ali Akhtar <ali@ali.actor>
2021-08-09 02:47:13 +0500

    Attempted to use react-speech-recognition - not working

--- genie-app/static/genie-ts/src/App.tsx
+++ genie-app/static/genie-ts/src/App.tsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { invoke } from '@forge/bridge';
+import Dictaphone from "./Dictaphone";
 
 function App() {
     const [data, setData] = useState(null);
 
-    useEffect(() => {
+    /*useEffect(() => {
         // @ts-ignore
         invoke('getText', { example: 'my-invoke-variable' }).then(setData);
-    }, []);
+    }, []);*/
 
     return (
         <div>
-            {data ? data : 'Loading...'}
+            <Dictaphone />
         </div>
     );
 }
