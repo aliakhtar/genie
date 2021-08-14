6ff24cd14bb1c42e4a623dcd345cd714cb18973a
Ali Akhtar <ali@ali.actor>
2021-08-15 03:43:46 +0500

    Added react dependency for Dictaphone.jsx - basic transcription working via api

--- genie-frontend/src/App.tsx
+++ genie-frontend/src/App.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dictaphone from "./Dictaphone";
 
 function App() {
-  const popup = () => {
-      window.open('https://genie-frontend.storage.googleapis.com/index.html?reload=1111');
-  }
+
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={popup}>Open popup</button>
-
-      </header>
+        <Dictaphone />
     </div>
   );
 }
