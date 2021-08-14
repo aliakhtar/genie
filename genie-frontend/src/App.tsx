f22d6bebdaa9db3f3cd78e34e55aa23c604917f0
Ali Akhtar <ali@ali.actor>
2021-08-15 03:51:11 +0500

    Switched to using another voice component - muuuuuuuuuuuuuuch better

--- genie-frontend/src/App.tsx
+++ genie-frontend/src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Dictaphone from "./Dictaphone";
+import Dicta2 from "./Dicta2";
 
 function App() {
 
 
   return (
     <div className="App">
-        <Dictaphone />
+        <Dicta2 />
     </div>
   );
 }
