db934732d5ac4202e7bd3b17f77d512f50b183ef
Ali Akhtar <ali@ali.actor>
2021-08-24 21:14:21 +0500

    Added bg color to header

--- genie-app/static/genie-ts/src/nav/Header.tsx
+++ genie-app/static/genie-ts/src/nav/Header.tsx
@@ -3,7 +3,7 @@ import Icon from '../assets/Icon.svg';
 export default function () {
 
     return (
-        <header className="bg-black">
+        <header className="bg-jiraBlue">
             <img src={Icon} alt="Genie Logo" height={43} />
             <span>GENIE</span>
             <span>Perform complex Jira tasks, just by telling Genie what to do.</span>
