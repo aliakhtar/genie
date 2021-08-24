660e56acf797a8e357be9cf39c71f02cab590309
Ali Akhtar <ali@ali.actor>
2021-08-24 20:34:36 +0500

    Created first pass nav + header

--- genie-app/static/genie-ts/src/App.tsx
+++ genie-app/static/genie-ts/src/App.tsx
@@ -2,33 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { invoke } from '@forge/bridge';
 import { router } from '@forge/bridge';
 import CSS from 'csstype';
+import Header from "./nav/Header";
 
 
 
-const styles: CSS.Properties = {
-    width: "100%",
-    height: "100%"
-};
 
 function App() {
     const url = "wss://echo.websocket.org";
-    const [data, setData] = useState(null);
 
-    function testWebSocket()
-    {
-        const websocket = new WebSocket(url);
-        websocket.onopen = e=> {
-            console.log("ws opened", e);
-            console.log("sending ping");
-            websocket.send("ping");
-        }
-        websocket.onclose = e=> console.log("ws closed", e);
-        websocket.onmessage = e=> console.log("ws msg", e);
-        websocket.onerror = e=> console.log("ws error", e);
-
-        router.open('http://genie-frontend.storage.googleapis.com/index.html');
-
-    }
 
 
     /*useEffect(() => {
@@ -37,9 +18,9 @@ function App() {
     }, []);*/
 
     return (
-        <div style={styles} >
-            <button onClick={testWebSocket}>Test</button>
-        </div>
+        <>
+            <Header />
+        </>
     );
 }
 
---
+++ genie-app/static/genie-ts/src/nav/Header.tsx
@@ -0,0 +1,11 @@
+export default function () {
+
+    return (
+        <header>
+            <img src="../assets/Icon.svg" height={41} alt="Genie Logo" />
+            <span>GENIE</span>
+            <span>Perform complex Jira tasks, just by telling Genie what to do.</span>
+        </header>
+    );
+
+}
