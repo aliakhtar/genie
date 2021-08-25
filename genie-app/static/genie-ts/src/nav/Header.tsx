f3188780e6e52decbb09dfdcbcf9042265273c08
Ali Akhtar <ali@ali.actor>
2021-08-26 03:17:14 +0500

    improved header formatting

--- genie-app/static/genie-ts/src/nav/Header.tsx
+++ genie-app/static/genie-ts/src/nav/Header.tsx
@@ -3,10 +3,12 @@ import Icon from '../assets/Icon.svg';
 export default function () {
 
     return (
-        <header className="bg-jiraBlue rounded-xl py-5 px-10 text-white">
-            <img src={Icon} alt="Genie Logo" height={43} />
-            <span>GENIE</span>
-            <span>Perform complex Jira tasks, just by telling Genie what to do.</span>
+        <header className="bg-jiraBlue rounded-xl py-2.5 px-10 text-white">
+            <div>
+                <img src={Icon} alt="Genie Logo" height={43} className="inline" />
+                <span className="text-xl font-extrabold tracking-widest inline mx-2.5 my-3">GENIE</span>
+            </div>
+            <p className="my-5 px-5">Perform complex Jira tasks, just by telling Genie what to do.</p>
         </header>
     );
 
