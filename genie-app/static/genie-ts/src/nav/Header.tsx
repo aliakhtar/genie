3df16f90a55fbbdc85c0c0042d0ac053a7047625
Ali Akhtar <ali@ali.actor>
2021-08-26 03:07:39 +0500

    Added padding + text color to header

--- genie-app/static/genie-ts/src/nav/Header.tsx
+++ genie-app/static/genie-ts/src/nav/Header.tsx
@@ -3,7 +3,7 @@ import Icon from '../assets/Icon.svg';
 export default function () {
 
     return (
-        <header className="bg-jiraBlue">
+        <header className="bg-jiraBlue rounded-xl py-5 px-10 text-white">
             <img src={Icon} alt="Genie Logo" height={43} />
             <span>GENIE</span>
             <span>Perform complex Jira tasks, just by telling Genie what to do.</span>
