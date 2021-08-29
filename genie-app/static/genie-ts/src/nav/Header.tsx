f49e72724936694a761307e9c0c81c8848997cde
Ali Akhtar <ali@ali.actor>
2021-08-29 06:42:24 +0500

    Trying to re-align header

--- genie-app/static/genie-ts/src/nav/Header.tsx
+++ genie-app/static/genie-ts/src/nav/Header.tsx
@@ -1,14 +1,21 @@
 import Icon from '../assets/Icon.svg';
+import {url} from "inspector";
 
 export default function () {
 
+    const logoStyle = {
+        backgroundImage: Icon
+    };
     return (
         <header className="bg-jiraBlue rounded-xl py-2.5 px-10 text-white">
-            <div>
-                <img src={Icon} alt="Genie Logo" height={43} className="inline" />
-                <span className="text-xl font-extrabold tracking-widest inline mx-2.5 my-3">GENIE</span>
+            <div className="flex flex-row justify-start items-stretch">
+                <div className="min-h-full" style={logoStyle} />
+                <div className="flex flex-col">
+                    <p className="text-xl font-extrabold tracking-widest inline mx-2.5">GENIE</p>
+                    <p className="my-3 mx-2.5">Perform complex Jira tasks, just by telling Genie what to do.</p>
+                </div>
+
             </div>
-            <p className="my-5 px-5">Perform complex Jira tasks, just by telling Genie what to do.</p>
         </header>
     );
 
